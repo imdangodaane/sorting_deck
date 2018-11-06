b82b728ea41a6299fa44fe60acc35211ecea825a
nqui <imdangodaane@gmail.com>
2018-11-06 21:46:22 +0700

    asd

--- sorting_deck.py
+++ sorting_deck.py
@@ -3,45 +3,102 @@ import argparse
 
 
 def swap_next(pos, lst):
-    temp = lst[pos + 1]
-    lst[pos + 1] = lst[pos]
-    lst[pos] = temp
+    lst[pos], lst[pos + 1] = lst[pos + 1], lst[pos]
 
 
 def bubble_sort(lst):
+    for _ in lst:
+        for i in range(len(lst) - 1):
+            if lst[i] > lst[i + 1]:
+                swap_next(i, lst)
+                print(' '.join(str(x) for x in lst))
+
+
+def insert_sort(lst):
+    # Insertion Sort Type I
+    # sorted_list = [] + lst[0]
+    # for i in range(1, len(lst)):
+    #     for j in range(len(sorted_list)):
+    #         if lst[i] < sorted_list[j]:
+    #             sorted_list.insert(j, lst[i])
+    #             print(sorted_list)
+    #             break
+
+    # Insertion Sort Type II
     for i in range(len(lst) - 1):
-        for j in range(len(lst) - 1):
-            if lst[j] > lst[j + 1]:
-                swap_next(j, lst)
-                j += 1
-                print(lst)
+        if lst[i] > lst[i + 1]:
+            for j in range(i + 1):
+                if lst[i + 1] <= lst[j]:
+                    lst.insert(j, lst[i + 1])
+                    del(lst[i + 2])
+                    print(' '.join(str(x) for x in lst))
+                    break
 
 
-def insertion_sort(lst):
-    pass
+def partition(arr, low, high):
+    i = (low - 1)
+    pivot = arr[high]
+    for j in range(low, high):
+        if arr[j] <= pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    print('P:', pivot)
+    print(' '.join(str(x) for x in arr))
+    return (i + 1)
 
 
-def quick_sort(lst):
-    pass
+def quick_sort(arr, low, high):
+    if low < high:
+        pi = partition(arr, low, high)
+        quick_sort(arr, low, pi-1)
+        quick_sort(arr, pi+1, high)
 
 
 def merge_sort(lst):
-    pass
+    if len(lst) > 1:
+        m = int(len(lst)/2)
+        L = lst[:m]
+        R = lst[m:]
+        merge_sort(L)
+        merge_sort(R)
+        i = j = k = 0
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                lst[k] = L[i]
+                i += 1
+            else:
+                lst[k] = R[j]
+                j += 1
+            k += 1
+        while i < len(L):
+            lst[k] = L[i]
+            i += 1
+            k += 1
+        while j < len(R):
+            lst[k] = R[j]
+            j += 1
+            k += 1
+        print(' '.join(str(x) for x in lst))
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('N', nargs='+', action='store', type=int, help='an integer for the list to sort')
-    parser.add_argument('--algo', metavar='ALGO', default='bubble', help='specify which algorithm to use for sorting among [bubble|insert|quick|merge], default bubble')
-    parser.add_argument('--gui', action='store_true', help='visualise the algorithm in GUI mode')
+    parser.add_argument('N', nargs='+', action='store', type=int,
+                        help='an integer for the list to sort')
+    parser.add_argument('--algo', metavar='ALGO', default='bubble',
+                        help='specify which algorithm to use for sorting among\
+                        [bubble|insert|quick|merge], default bubble')
+    parser.add_argument('--gui', action='store_true',
+                        help='visualise the algorithm in GUI mode')
     args = parser.parse_args()
 
     if args.algo == 'bubble':
         bubble_sort(args.N)
     elif args.algo == 'insert':
-        insertion_sort(args.N)
+        insert_sort(args.N)
     elif args.algo == 'quick':
-        quick_sort(args.N)
+        quick_sort(args.N, 0, len(args.N) - 1)
     elif args.algo == 'merge':
         merge_sort(args.N)
 
